b258b44756d5f8a4cca9c69d161414b0620dced8
devbrammfirstwife <bramwellokothgmail.com>
2022-03-15 09:13:13 +0300

    commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFDBC1AD);
+const kSecondaryColor = Color(0xFF928490);
+const kContentColorLightTheme = Color(0xFF433E49);
+const kContentColorDarkTheme = Color(0xFFF3E8EB);
+const kWarningColor = Color(0xFFF3BB1C);
+const kErrorColor = Color(0xFFF03738);
+
+const kDefaultPadding = 20.0;
+
